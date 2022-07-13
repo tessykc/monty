ce359248e0a67b67416345ca0c46e3c320ad079a
tessykc <tessykc@gmail.com>
2022-07-13 12:51:33 -0700

    Betty Checked

--- free_stack.c
+++ free_stack.c
@@ -1,59 +1,29 @@
 #include "monty.h"
 
-
-
 /**
+ * free_stack - frees the stack on exit
+ * @status: exit status
+ * @arg: double pointer to the stack
  *
- *  * free_stack - frees the stack on exit
- *
- *   * @status: exit status
- *
- *    * @arg: double pointer to the stack
- *
- *     *
- *
- *      * Return: void
- *
- *       */
-
+ * Return: void
+ */
 void free_stack(int status, void *arg)
-
 {
+	stack_t **stack;
+	stack_t *next;
+	(void)status;
 
-		stack_t **stack;
-
-			stack_t *next;
-
-
-
-				(void)status;
-
-
-
-					stack = (stack_t **)arg;
-
-						if (*stack)
-
-								{
-
-											(*stack)->prev->next = NULL;
-
-													(*stack)->prev = NULL;
-
-														}
-
-							while (*stack != NULL)
-
-									{
-
-												next = (*stack)->next;
-
-														free(*stack);
-
-																*stack = next;
-
-																	}
-
-								var.stack_len = 0;
-
+	stack = (stack_t **)arg;
+	if (*stack)
+	{
+		(*stack)->prev->next = NULL;
+		(*stack)->prev = NULL;
+	}
+	while (*stack != NULL)
+	{
+		next = (*stack)->next;
+		free(*stack);
+		*stack = next;
+	}
+	var.stack_len = 0;
 }
--- m_fs_close.c
+++ m_fs_close.c
@@ -1,35 +1,16 @@
 #include "monty.h"
 
-
-
 /**
+ * m_fs_close - close file stream
+ * @status: status passed to exit
+ * @arg: pointer to file stream
  *
- *  * m_fs_close - close file stream
- *
- *   * @status: status passed to exit
- *
- *    * @arg: pointer to file stream
- *
- *     *
- *
- *      * Return: void
- *
- *       */
-
+ * Return: void
+ */
 void m_fs_close(int status, void *arg)
-
 {
-
-		FILE *fs;
-
-
-
-			(void)status;
-
-
-
-				fs = (FILE *) arg;
-
-					fclose(fs);
-
+	FILE *fs;
+	(void)status;
+	fs = (FILE *) arg;
+	fclose(fs);
 }
