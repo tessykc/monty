7cfba3feb71dd627d719eddcde94e70a848841d2
tessykc <tessykc@gmail.com>
2022-07-15 19:39:19 -0700

    changes

--- push.c
+++ push.c
@@ -9,16 +9,16 @@
 */
 static int check_for_digit(char *arg)
 {
-int i;
+	int i;
 
-for (i = 0; arg[i]; i++)
-{
-if (arg[i] == '-' && i == 0)
-continue;
-if (isdigit(arg[i]) == 0)
-return (1);
-}
-return (0);
+	for (i = 0; arg[i]; i++)
+	{
+		if (arg[i] == '-' && i == 0)
+			continue;
+		if (isdigit(arg[i]) == 0)
+			return (1);
+	}
+	return (0);
 }
 
 /**
@@ -30,23 +30,22 @@ return (0);
 */
 void m_push(stack_t **stack, unsigned int line_number)
 {
-char *arg;
-int n;
-
-arg = strtok(NULL, "\n\t\r ");
-if (arg == NULL || check_for_digit(arg))
-{
-dprintf(STDOUT_FILENO,
-"L%u: usage: push integer\n",
-line_number);
-exit(EXIT_FAILURE);
-}
-n = atoi(arg);
-if (!add_node(stack, n))
-{
-dprintf(STDOUT_FILENO, "Error: malloc failed\n");
-exit(EXIT_FAILURE);
-
-}
-var.stack_len++;
+	char *arg;
+	int n;
+	
+	arg = strtok(NULL, "\n\t\r ");
+	if (arg == NULL || check_for_digit(arg))
+	{
+		dprintf(STDOUT_FILENO,
+				"L%u: usage: push integer\n",
+				line_number);
+		exit(EXIT_FAILURE);
+	}
+	n = atoi(arg);
+	if (!add_node(stack, n))
+	{
+		dprintf(STDOUT_FILENO, "Error: malloc failed\n");
+		exit(EXIT_FAILURE);
+	}
+	var.stack_len++;
 }
